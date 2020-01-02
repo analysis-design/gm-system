2fec936931bea57f66780a5c80ba47b4a2a32f64
xsk0802 <1580286943@qq.com>
2020-01-02 09:51:44 +0800

    xushaokang:添加了GroupMenberService接口，Impl实现类。 HeavyToolService接口，Impl实现类。

---
+++ src/main/java/com/xzit/garden/service/impl/HeavyToolServiceImpl.java
@@ -0,0 +1,9 @@
+package com.xzit.garden.service.impl;
+
+import com.xzit.garden.service.HeavyToolService;
+
+/**
+ * Created by Xsk on 2020/1/2.
+ */
+public class HeavyToolServiceImpl implements HeavyToolService {
+}
