46b572b56ea86d18953562f3bf72b3112ab91fa1
xsk0802 <1580286943@qq.com>
2020-01-02 09:46:27 +0800

    xushaokang添加了DepartmentService接口，Impl实现类。 ExpenseCostService接口，Impl实现类。 GroupService接口，Impl实现类。

---
+++ src/main/java/com/xzit/garden/service/impl/DepartmentServiceImpl.java
@@ -0,0 +1,9 @@
+package com.xzit.garden.service.impl;
+
+import com.xzit.garden.service.DepartmentService;
+
+/**
+ * Created by Xsk on 2020/1/2.
+ */
+public class DepartmentServiceImpl implements DepartmentService {
+}
