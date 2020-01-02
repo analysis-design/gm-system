5dc0258fe1846084878f5e67e0178e6475977ce9
xsk0802 <1580286943@qq.com>
2020-01-02 10:05:28 +0800

    xushaokang:添加了OrderService接口，Impl实现类。 OvertimeCostService接口，Impl实现类。

---
+++ src/main/java/com/xzit/garden/service/impl/OrderServiceImpl.java
@@ -0,0 +1,9 @@
+package com.xzit.garden.service.impl;
+
+import com.xzit.garden.service.OrderService;
+
+/**
+ * Created by Xsk on 2020/1/2.
+ */
+public class OrderServiceImpl implements OrderService {
+}
