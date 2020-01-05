d074073933ca8968fdd1a78561c34b43f2026f8a
liu <837238676@qq.com>
2020-01-05 19:54:22 +0800

    刘晓帅增加了订单管理

--- src/main/java/com/xzit/garden/mapper/OrderMapper.java
+++ src/main/java/com/xzit/garden/mapper/OrderMapper.java
@@ -8,28 +8,28 @@ import java.util.List;
 @Mapper
 public interface OrderMapper {
 
-    @Insert("insert into orders (name,budgetId,budgetTotal,discount,prepaid,payState," +
+    @Insert("insert into `order` (name,budgetId,budgetTotal,discount,prepaid,payState," +
             "paymentMethod,paymentTotal,createTime,endTime,projectId) values (#{name}," +
             "#{budgetId},#{budgetTotal},#{discount},#{prepaid},#{payState},#{paymentMethod},#{paymentTotal},#{createTime},#{endTime},#{projectId})")
     Integer insert(Order order);
 
-    @Delete("delete from orders where id=#{id}")
+    @Delete("delete from `order` where id=#{id}")
     Integer delete(@Param("id")Long id);
 
-    @Update("update orders set name=#{name},budgetId=#{budgetId},budgetTotal=#{budgetTotal},discount=#{discount}," +
+    @Update("update `order` set name=#{name},budgetId=#{budgetId},budgetTotal=#{budgetTotal},discount=#{discount}," +
             "prepaid=#{prepaid},payState=#{payState},paymentMethod=#{paymentMethod},paymentTotal=#{paymentTotal}," +
             "createTime=#{createTime},endTime=#{endTime},projectId=#{projectId} where id=#{id}")
     Integer update(Order order);
 
-    @Select("select * from orders where id=#{id}")
+    @Select("select * from `order` where id=#{id}")
     Order findById(@Param("id")Long id);
 
-    @Select("select * from orders")
+    @Select("select * from `order`")
     List<Order> findAll();
 
-    @Select("select * from orders where projectId in (select id from project where name like '%${projectName}%')")
+    @Select("select * from `order` where projectId in (select id from project where name like '%${projectName}%')")
     List<Order> findByProjectName(@Param("projectName")String name);
 
-    @Select("select * from orders where projectId=#{projectId}")
+    @Select("select * from `order` where projectId=#{projectId}")
     Order findByProjectId(@Param("projectId") Long id);
 }
